81836906a5c41109cb383ab4e688aeaf8debc5f3
rtkilian-movember <rhys.kilian12@gmail.com>
2021-08-15 17:10:29 +1000

    Add links to Medium post and GitHub repo

--- sentiment_analyser.py
+++ sentiment_analyser.py
@@ -2,8 +2,11 @@ import streamlit as st
 from transformers import pipeline
 
 st.title('Sentiment Analyser App')
-st.write('This app uses the Hugging Face transformers [sentiment analyser](https://huggingface.co/course/chapter1/3?fw=tf) to clasify the sentiment of your input as postive or negative. The web app is built using [Streamlit](https://docs.streamlit.io/en/stable/getting_started.html).')
-st.write('*Note: it will take approximately 30 seconds to run the app.*')
+st.write('This app uses the Hugging Face Transformers [sentiment analyser](https://huggingface.co/course/chapter1/3?fw=tf) library to clasify the sentiment of your input as postive or negative. The web app is built using [Streamlit](https://docs.streamlit.io/en/stable/getting_started.html).')
+st.write(
+    'To find out how this app was developed, please check out my [Medium post](https://medium.com/@rtkilian/deploy-and-share-your-sentiment-analysis-app-using-streamlit-sharing-2ba3ca6a3ead). To see my source code, have a look at my [GitHub repo](https://github.com/rtkilian/streamlit-huggingface).')
+
+st.write('*Note: it will take up to 30 seconds to run the app.*')
 
 form = st.form(key='sentiment-form')
 user_input = form.text_area('Enter your text')
